78d4adabbeaf25bc737165b68c39f25c817c11b4
Stephen Darlington <stephen.darlington@gridgain.com>
2024-11-25 17:44:31 +0000

    Update with new SQL syntax in GG 9.0.9

--- config/media_store.sql
+++ config/media_store.sql
@@ -38,7 +38,7 @@ CREATE TABLE Artist
     ArtistId INT,
     Name VARCHAR(120),
     PRIMARY KEY (ArtistId)
-) WITH primary_zone=chinook;
+) primary zone chinook;
 
 CREATE TABLE Album
 (
@@ -46,7 +46,7 @@ CREATE TABLE Album
     Title VARCHAR(160),
     ArtistId INT,
     PRIMARY KEY (AlbumId, ArtistId)
-) COLOCATE BY (artistid) WITH primary_zone=chinook ;
+) COLOCATE BY (artistid) primary zone chinook ;
 
 CREATE TABLE Customer
 (
@@ -64,7 +64,7 @@ CREATE TABLE Customer
     Email VARCHAR(60),
     SupportRepId INT,
     PRIMARY KEY (CustomerId)
-) WITH primary_zone=chinook;
+) primary zone chinook;
 
 CREATE TABLE Invoice
 (
@@ -78,7 +78,7 @@ CREATE TABLE Invoice
     BillingPostalCode VARCHAR(10),
     Total DECIMAL(10,2),
     PRIMARY KEY  (InvoiceId, CustomerId)
-) COLOCATE BY (CustomerId) WITH primary_zone=chinook;
+) COLOCATE BY (CustomerId) primary zone chinook;
 
 CREATE TABLE InvoiceLine
 (
@@ -89,7 +89,7 @@ CREATE TABLE InvoiceLine
     UnitPrice DECIMAL(10,2),
     Quantity INT,
     PRIMARY KEY (InvoiceLineId, CustomerId)
-) COLOCATE BY (CustomerId) WITH primary_zone=chinook;
+) COLOCATE BY (CustomerId) primary zone chinook;
 
 CREATE TABLE Employee
 (
@@ -109,28 +109,28 @@ CREATE TABLE Employee
     Fax VARCHAR(24),
     Email VARCHAR(60),
     PRIMARY KEY (EmployeeId)
-) WITH primary_zone=chinook;
+) primary zone chinook;
 
 CREATE TABLE Genre
 (
     GenreId INT,
     Name VARCHAR(120),
     PRIMARY KEY (GenreId)
-) WITH primary_zone=ChinookReplicated;
+) primary zone chinookReplicated;
 
 CREATE TABLE MediaType
 (
     MediaTypeId INT,
     Name VARCHAR(120),
     PRIMARY KEY (MediaTypeId)
-) WITH primary_zone=ChinookReplicated;
+) primary zone chinookReplicated;
 
 CREATE TABLE Playlist
 (
     PlaylistId INT,
     Name VARCHAR(120),
     PRIMARY KEY  (PlaylistId)
-) WITH primary_zone=chinook;
+) primary zone chinook;
 
 CREATE TABLE PlaylistTrack
 (
@@ -139,7 +139,7 @@ CREATE TABLE PlaylistTrack
     ArtistId INT,
     Dummy TINYINT, /* to fix the issue saying that the table must have at least one non-primary key column */
     PRIMARY KEY (PlaylistId, TrackId, ArtistId)
-) COLOCATE BY (ArtistId) WITH primary_zone=chinook;
+) COLOCATE BY (ArtistId) primary zone chinook;
 
 CREATE TABLE Track
 (
@@ -154,7 +154,7 @@ CREATE TABLE Track
     Bytes INT,
     UnitPrice DECIMAL(10,2),
     PRIMARY KEY (TrackId)
-) WITH primary_zone=chinook;
+) primary zone chinook;
 
 INSERT INTO Genre (GenreId, Name) VALUES (1, 'Rock');
 INSERT INTO Genre (GenreId, Name) VALUES (2, 'Jazz');
